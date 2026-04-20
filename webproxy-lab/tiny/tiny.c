6ee0c6d96c69da9464fa2af42126d24da31c3a83
dolggul2 <dolggul2@gmail.com>
2026-04-20 21:09:43 +0900

    feat: implement parse_uri function

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -212,5 +212,35 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
   Wait(NULL);
 }
 
-// TODO: implement
-// int parse_uri(char *uri, char *filename, char *cgiargs);
+int parse_uri(char *uri, char *filename, char *cgiargs)
+{
+  char *ptr;
+
+  if (!strstr(uri, "cgi-bin"))
+  {
+    strcpy(cgiargs, "");
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    if (uri[strlen(uri) - 1] == '/')
+    {
+      strcat(filename, "home.html");
+    }
+    return 1;
+  }
+  else
+  {
+    ptr = index(uri, '?');
+    if (ptr)
+    {
+      strcpy(cgiargs, ptr + 1);
+      *ptr = '\0';
+    }
+    else
+    {
+      strcpy(cgiargs, "");
+    }
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    return 0;
+  }
+}
