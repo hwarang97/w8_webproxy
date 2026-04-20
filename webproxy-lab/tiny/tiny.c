56a4768499970f652d31ff1cd93c861954a55673
dolggul2 <dolggul2@gmail.com>
2026-04-20 13:52:50 +0900

    feat: skeleton doit function

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -44,3 +44,70 @@ int main(int argc, char **argv)
     Close(connfd); // line:netp:tiny:close
   }
 }
+
+void doit(int fd)
+{
+  // 소켓을 읽어들이고, static인지 dynamic인지를 판단한 뒤에, serve_static() 또는 serve_dynamic()을 호출한다.
+
+  rio_t rio;
+  int is_static;
+  struct stat sbuffer;
+  char buffer[MAXLINE];
+  char method[16]; // any method length is not longher than 6.
+  char uri[MAXLINE];
+  char version[16];
+  char filename[MAXLINE];
+  char cgiargs[MAXLINE];
+
+  Rio_readinitb(&rio, fd);
+  Rio_readlineb(&rio, buffer, MAXLINE);
+  sscanf(buffer, "%s %s %s", method, uri, version);
+
+  if (strcasecmp(method, "GET"))
+  {
+    //  clienterror();  // TODO: implement clienterror function which flush error message with socket
+    return;
+  }
+
+  // empty rio internal buffer, it's not essential but conventinal
+  // we don't need to reuse connfd, so empty rio buffer later than GET if statement
+  // TODO: implement read_requesthdrs
+  read_requesthdrs(&rio);
+
+  // parse uri to detemine whether static request, filename, cgiargs
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuffer) < 0)
+  {
+    // clienterror(); // TODO: implement clienterror function which flush error message with socket
+    return;
+  }
+
+  // branch static, dynamic
+  if (is_static)
+  {
+    if (!(S_ISREG(sbuffer.st_mode)) || !(S_IRUSR & sbuffer.st_mode))
+    {
+      // clienterror(); // TODO: implement clienterror function which flush error message with socket
+      return;
+    }
+    serve_static(fd, filename, sbuffer.st_size); // TODO: implement serve_static
+  }
+  else
+  {
+    if (!(S_ISREG(sbuffer.st_mode)) || !(S_IXUSR & sbuffer.st_mode))
+    {
+      // clienterror(); // TODO: implement clienterror function which flush error message with socket
+      return;
+    }
+    serve_dynamic(fd, filename, cgiargs); // TODO: implement serve_dynamic
+  }
+}
+
+// TODO: implement
+// void read_requesthdrs(rio_t *rp);
+// int parse_uri(char *uri, char *filename, char *cgiargs);
+// void serve_static(int fd, char *filename, int filesize);
+// void get_filetype(char *filename, char *filetype);
+// void serve_dynamic(int fd, char *filename, char *cgiargs);
+// void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
+//                  char *longmsg);
