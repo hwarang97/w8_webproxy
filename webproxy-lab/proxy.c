92eae254ed4b48a27a5365c8473f453bacb1950c
dolggul2 <dolggul2@gmail.com>
2026-04-21 20:10:18 +0900

    feat: main skeleton

--- webproxy-lab/proxy.c
+++ webproxy-lab/proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -9,8 +10,40 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main()
+int main(int argc, char **argv)
 {
-  printf("%s", user_agent_hdr);
-  return 0;
+  // 리스닝 소켓으부터 연결 소켓 만들기 (루프)
+
+  // connfd 에서 요청줄과 헤더 읽기
+
+  // 클라이언트에게 HTTP 응답 돌려주기
+
+  // 연결 소켓 닫기
+
+  int listenfd;
+  int connfd;
+  struct sockaddr_storage clientaddr;
+  socklen_t clientlen;
+  char client_hostname[MAXLINE];
+  char client_port[MAXLINE];
+
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  listenfd = Open_listenfd(argv[1]);
+  while (1)
+  {
+    clientlen = sizeof(struct sockaddr_storage);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE,
+                client_port, MAXLINE, 0);
+    printf("Connected to (%s, %s)\n", client_hostname, client_port);
+    echo(connfd);
+    Close(connfd);
+  }
+  // printf("%s", user_agent_hdr);
+  exit(0);
 }
